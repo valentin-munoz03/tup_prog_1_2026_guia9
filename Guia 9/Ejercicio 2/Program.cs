4accb0efb9d6d4375861b8daeff76b622026f9e2
valentin-munoz03 <valentin.munoz03@gmail.com>
2026-06-11 19:17:49 -0300

    Ejercicio 2

--- Guia 9/Ejercicio 2/Program.cs
+++ Guia 9/Ejercicio 2/Program.cs
@@ -2,14 +2,60 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Ejercicio_2
 {
     internal class Program
     {
+        static Servicio servicio = new Servicio();
+        static int MostrarPantallaSolicitarOpcionMenu()
+        {
+            int opcion;
+            Console.WriteLine("1- Iniciar monto edad");
+            Console.WriteLine("2- Solicitar edad por niña");
+            Console.WriteLine("3- Mostrar monto y porcentajes que corresponde a cada niña");
+            Console.WriteLine("4- Salir");
+            opcion = Convert.ToInt32(Console.ReadLine());
+            return opcion;
+        }
+        static void MostrarPantallaSolicitarMontoARepartir()
+        {
+            int monto;
+            Console.WriteLine("Ingrese el monto a repartir");
+            monto = Convert.ToInt32(Console.ReadLine());
+            servicio.MontoARepartir(monto);
+        }
+        static void MostrarPantallaSolicitarEdadesDeLasNiñas()
+        {
+            servicio.RegistrarEdad();
+        }
+        static void MostrarPantallaCalcularMostrarMontoYPorcentajePorNiña()
+        {
+            servicio.CalcularMontosYPorcentajesARepartir();
+            Console.WriteLine($"El monto de la niña con edad {servicio.edad0} es {servicio.monto0} y el porcentaje es {servicio.porcentaje0}%");
+            Console.WriteLine($"El monto de la niña con edad {servicio.edad1} es {servicio.monto1} y el porcentaje es {servicio.porcentaje1}%");
+            Console.WriteLine($"El monto de la niña con edad {servicio.edad2} es {servicio.monto2} y el porcentaje es {servicio.porcentaje2}%");
+            Console.WriteLine($"El monto de la niña con edad {servicio.edad3} es {servicio.monto3} y el porcentaje es {servicio.porcentaje3}%");
+        }
+
         static void Main(string[] args)
         {
+            int opcion;
+            do
+            {
+                opcion = MostrarPantallaSolicitarOpcionMenu();
+                switch (opcion)
+                {
+                    case 1: MostrarPantallaSolicitarMontoARepartir(); break;
+                    case 2: MostrarPantallaSolicitarEdadesDeLasNiñas(); break;
+                    case 3: MostrarPantallaCalcularMostrarMontoYPorcentajePorNiña(); break;
+                    case 4: Console.WriteLine("SALIENDO"); break;
+                }
+
+            }
+            while (opcion != 4);
         }
     }
 }
---
+++ Guia 9/Ejercicio 2/Servicio.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2
+{
+    internal class Servicio
+    {
+        public int edad0; 
+        public int edad1;
+        public int edad2;
+        public int edad3;
+        public double Monto;
+        public double porcentaje0;
+        public double porcentaje1;
+        public double porcentaje2;
+        public double porcentaje3;
+        public double monto0;
+        public double monto1;
+        public double monto2;
+        public double monto3;
+        public void MontoARepartir(double monto)
+        {
+            Monto = monto;
+        }
+        public void RegistrarEdad()
+        {
+            Console.WriteLine("Ingrese la edad de la primer niña");
+            edad0 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese la edad de la segunda niña");
+            edad1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese la edad de la tercer niña");
+            edad2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese la edad de la cuarta niña");
+            edad3 = Convert.ToInt32(Console.ReadLine());
+        }
+        public void CalcularMontosYPorcentajesARepartir()
+        {
+            int SumaEdades;
+            SumaEdades = edad0 + edad1 + edad2 + edad3;
+            porcentaje0 = (edad0 * 100) / SumaEdades;
+            porcentaje1 = (edad1 * 100) / SumaEdades;
+            porcentaje2 = (edad2 * 100) / SumaEdades;
+            porcentaje3 = (edad3 * 100) / SumaEdades;
+            monto0 = (Monto * porcentaje0) / 100;
+            monto1 = (Monto * porcentaje1) / 100;
+            monto2 = (Monto * porcentaje2) / 100;
+            monto3 = (Monto * porcentaje3) / 100;
+        }
+    }
+}
