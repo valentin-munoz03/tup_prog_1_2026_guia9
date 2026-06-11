32e885401c03c7142925414d172497a8f2125ff1
valentin-munoz03 <valentin.munoz03@gmail.com>
2026-06-10 21:47:53 -0300

    Ejercicio 1

---
+++ Guia 9/Ejercicio 1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+    internal class Program
+    {
+        static Servicio servicio;
+        static int MostrarPantallaSolicitarOpcionMenu()
+        {
+            Console.WriteLine("1- Procesar un solo número");
+            Console.WriteLine("2- Procesar varios números");
+            Console.WriteLine("3- Mostrar máximo y mínimo.");
+            Console.WriteLine("4- Mostrar promedio.");
+            Console.WriteLine("5- Mostrar cantidad de números ingresados.");
+            Console.WriteLine("6- Reiniciar variables");
+            Console.WriteLine("7- Salir");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+            return opcion;
+        }
+        static void MostrarIniciarVariables()
+        {
+            servicio = new Servicio();
+            Console.WriteLine("Variables reiniciadas.");
+        }
+        static void MostrarPantallaSolicitarNumero()
+        {
+            Console.WriteLine("Ingrese un numero");
+            int num = Convert.ToInt32(Console.ReadLine());
+            servicio.RegistrarValor(num);
+        }
+        static void MostrarPantallaSolicitarVariosNumeros()
+        {
+            int num;
+            Console.WriteLine("Ingrese VARIOS numeros, -1 para salir");
+            num = Convert.ToInt32(Console.ReadLine());
+            servicio.RegistrarValor(num);
+            while (num != -1)
+            {
+                Console.WriteLine("Ingrese VARIOS numeros, -1 para salir");
+                num = Convert.ToInt32(Console.ReadLine());
+                servicio.RegistrarValor(num);
+            }
+        }
+        static void MostrarPantallaMaximoYMinimo()
+        {
+            Console.WriteLine($"El numero maximo es {servicio.maximo} y el numero minimo es {servicio.minimo}");
+        }
+        static void MostrarPantallaCalcularYMostrarPromedio()
+        {
+            double promedio = servicio.CalcularPromedio();
+            Console.WriteLine($"El promedio de los numeros ingresados es {promedio}");
+        }
+        static void MostrarCantidadDeNumeros()
+        {
+            Console.WriteLine($"La cantidad de numeros ingresados es {servicio.contador}");
+        }
+        static void Main(string[] args)
+        {
+            servicio = new Servicio();
+            int opcion;
+            do
+            {
+                opcion = MostrarPantallaSolicitarOpcionMenu();
+                switch (opcion)
+                {
+                    case 1: MostrarPantallaSolicitarNumero(); break;
+                    case 2: MostrarPantallaSolicitarVariosNumeros(); break;
+                    case 3: MostrarPantallaMaximoYMinimo(); break;
+                    case 4: MostrarPantallaCalcularYMostrarPromedio(); break;
+                    case 5: MostrarCantidadDeNumeros(); break;
+                    case 6: MostrarIniciarVariables(); break;
+                    case 7: Console.WriteLine("Saliendo..."); break;
+                }
+            }
+            while (opcion != 7);
+        }
+
+    }
+}
---
+++ Guia 9/Ejercicio 1/Servicio.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+    internal class Servicio
+    {
+        private int acumulador;
+        public int contador;
+        public int maximo;
+        public int minimo; 
+        public Servicio()
+        {
+            acumulador = 0;
+            contador = 0;
+            maximo = 0;
+            minimo = 9999;
+        }
+        public double CalcularPromedio()
+        {
+            int promedio;
+            promedio = acumulador / contador; 
+            return promedio;
+        }
+        public void RegistrarValor(int valor)
+        {
+           acumulador += valor;
+            contador++; 
+            if (valor>maximo)
+            {
+                maximo = valor;
+            }
+            if (valor<minimo)
+            {
+                minimo= valor;
+            }
+        }
+    }
+}
